dbbc2d8afa9f80be3a4c71eaf4229358120359a9
Brittany Rausch and Connor Cummings <connor@>
2016-03-30 15:06:18 -0700

    finalized logic

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,18 @@ $(document).ready(function() {
     for (var i = 0; i < splits.length; i++){
       var commas = splits[i].split("");
       if(commas[0] == "a" || commas[0] == "e" || commas[0] == "i" || commas[0] == "o" || commas[0] == "u"){
-        splits[i] = splits[i]+'ay';
+        splits[i] = splits[i]+'way';
+        console.log(splits[i]);
       } else if (commas[1] == "a" || commas[1] == "e" || commas[1] == "i" || commas[1] == "o" || commas[1] == "u")
-
-
-      var splitso = splits[i].split(commas[0]);
-
-      splits[i] = splitso[1] + commas[0]+'ay';
-      console.log(splits[i]);
+      {
+        var splitso = splits[i].split(commas[0]);
+        splits[i] = splitso[1] + commas[0]+'ay';
+        console.log(splits[i]);
+      } else if (commas[2] == "a" || commas[2] == "e" || commas[2] == "i" || commas[2] == "o" || commas[2] == "u"){
+        var splitso = splits[i].split(commas[1]);
+        splits[i] = splitso[1] + commas[0] + commas[1]+'ay';
+        console.log(splits[i]);
+      }
     }
-
   });
 });
